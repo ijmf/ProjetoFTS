6a19905e8414c84fbc3d2a7269266184f1d58b80
Ivan Ferreira <ivanjuniomf@gmail.com>
2024-09-22 12:36:26 -0300

    Atualização: Inclusão de conceito sobre API, Testes primários utilizando Swagger e Postman. Criação do arquivo TesteUser. Inclusão das dependências: Rest assured e Hamcrest.

---
+++ src/test/java/apiTest/TesteUser.java
@@ -0,0 +1,17 @@
+// nome do Pacote
+package apiTest;
+
+//Bibliotecas
+import org.junit.jupiter.api.Test;
+
+//Classe
+public class TesteUser {
+    //Atributos
+
+    //Funções e Metódos
+    @Test
+    public void testarIncluirUser(){
+
+    }
+}
+
--- src/test/java/unitTest/TesteCalculadora.java
+++ src/test/java/unitTest/TesteCalculadora.java
@@ -1,7 +1,8 @@
+package unitTest;
+
 import br.com.iterasys.Calculadora;
-import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
-import org.junit.jupiter.params.*;
+
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvFileSource;
